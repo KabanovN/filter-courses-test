d3417550ca011cc2f011b4a4c98989edc24837ea
Nikita <kabanov.n.v@mail.ru>
2022-06-10 22:33:20 +0300

    refactoring to conditions

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-// Список курсов
+// input
 let courses = [
     { name: 'Courses in England', prices: [0, 100] },
     { name: 'Courses in Germany', prices: [500, null] },
@@ -10,59 +10,42 @@ let courses = [
     { name: 'Courses in France', prices: [null, null] },
 ];
 
-// Варианты цен (фильтры), которые ищет пользователь
 let requiredRange1 = [null, 200];
 let requiredRange2 = [100, 350];
 let requiredRange3 = [200, null];
 
-//фильтрация
-function filterByRange(item = [], range = []) {
-    if (range[0] === null && range[1] === null) {
-        return true;
-    }
+//decision
+function filterByRange(arr, prop, range) {
+    function filterConditions(prop = [], range = []) {
+        if (range[0] === null && range[1] === null) return true;
 
-    if (range[0] === null) {
-        if (item[1] !== null) {
-            return range[1] >= item[1];
-        } else {
-            return false;
+        if (range[0] === null) {
+            if (prop[1] !== null) {
+                return range[1] >= prop[1];
+            } else {
+                return false;
+            }
         }
+
+        if (range[1] === null) return range[0] <= prop[0];
+
+        if (prop[1] === null) return false;
+
+        return range[0] <= prop[0] && range[1] >= prop[1];
     }
 
-    if (range[1] === null) {
-        return range[0] <= item[0];
+    function sortingByPrice(a, b) {
+        if (a > b) {
+            return 1;
+        }
+        if (a < b) {
+            return -1;
+        }
+        return 0;
     }
 
-    if (item[1] === null) {
-        return false;
-    }
-
-    return range[0] <= item[0] && range[1] >= item[1];
+    const filteredArray = arr.filter((elem) =>
+        filterConditions(elem[prop], range)
+    );
+    return filteredArray.sort((a, b) => sortingByPrice(a[prop][0], b[prop][0]));
 }
-
-const filteredCourses1 = courses.filter((item) =>
-    filterByRange(item.prices, requiredRange1)
-);
-const filteredCourses2 = courses.filter((item) =>
-    filterByRange(item.prices, requiredRange2)
-);
-const filteredCourses3 = courses.filter((item) =>
-    filterByRange(item.prices, requiredRange3)
-);
-
-//сортировка
-function sortingByPrice(a, b) {
-    if (a > b) {
-        return 1;
-    }
-    if (a < b) {
-        return -1;
-    }
-    return 0;
-}
-
-const sortingCourses = courses.sort((a, b) =>
-    sortingByPrice(a.prices[0], b.prices[0])
-);
-
-const sortingCourses2 = courses.sort((a, b) => a - b);
